ce6a3892a1fbc5e1d732109eb18901302acf8582
Ekky Pramana <ekkypramana@gmail.com>
2016-04-20 21:33:18 +0700

    Add Question.vue component

---
+++ src/components/Question.vue
@@ -0,0 +1,29 @@
+
+
+<template>
+  <div class="mdl-grid">
+    <div id="question-box" class="mdl-cell--6-col
+    mdl-cell--3-offset-desktop
+    mdl-card mdl-shadow--2dp">
+    <div class="mdl-supporting-text">
+      <a class="title mdl-navigation__link" v-link="'#'"><h4>Question<span class="mdl-badge" data-badge="12"></span></h4></a>
+
+    </div>
+    <div class="mdl-card__actions mdl-card--border">
+  </div>
+  <div class>
+    <a class="desc mdl-navigation__link" v-link="'#'">User</a></a> bertanya TIME yang lalu.
+  </div>
+    </div>
+  </div>
+</template>
+
+<style>
+  #question-box {
+    min-height: 130px;
+  }
+
+</style>
+
+<script>
+</script>
