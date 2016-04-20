75a4d548f511ff9d0d7c8c8febdcab2bb08b984f
Ekky Pramana <ekkypramana@gmail.com>
2016-04-20 11:07:11 +0700

    Component style cleanup

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="mdl-grid">
-    <div class="mdl-cell mdl-cell--6-col mdl-cell--3-offset-desktop" style="margin-top:170px; text-align:center;">
+    <div class="heading mdl-cell mdl-cell--6-col mdl-cell--3-offset-desktop">
       <img src="/assets/arlin_logo_color.svg" style="width: 60%;"/>
       <h5 style="">Ke sana ke mari, tanya di sini.</h5>
       <br>
       <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-        <a class="mdl-navigation__link" v-link="'signin'" style="color:white;">MASUK</a>
+        <a class="mdl-navigation__link" v-link="'signin'">MASUK</a>
       </button>
       <p><a href="#">Atau daftar sekarang</a></p>
     </div>
   </div>
 </template>
 
+<style>
+  .heading {
+    margin-top:170px;
+    text-align:center;
+  }
+
+  .heading > button a {
+    color:white;
+  }
+</style>
 
  <script>
  </script>
