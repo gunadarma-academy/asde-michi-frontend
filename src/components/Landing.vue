20b5ebd3106f64b52440117bc94cb7a828bc32d1
M Haidar Hanif <git@mhaidarhanif.com>
2016-04-21 01:35:59 +0700

    Change 'button' to 'a' so it works with v-link
    Resolve #3

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -4,10 +4,8 @@
       <img src="/assets/arlin_logo_color.svg" style="width: 60%;"/>
       <h5 style="">Ke sana ke mari, tanya di sini.</h5>
       <br>
-      <button class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent">
-        <a class="mdl-navigation__link" v-link="'signin'">MASUK</a>
-      </button>
-      <p><a href="#">Atau daftar sekarang</a></p>
+      <a class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent mdl-navigation__link" v-link="'signin'">MASUK</a>
+      <a class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect mdl-button--accent mdl-navigation__link" v-link="'signup'">DAFTAR</a>
     </div>
   </div>
 </template>
@@ -17,7 +15,6 @@
     margin-top:170px;
     text-align:center;
   }
-
   .heading > button a {
     color:white;
   }
