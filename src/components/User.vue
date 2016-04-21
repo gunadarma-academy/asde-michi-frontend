4169de41b830ebd0d53cbf113450a6566c569145
Ekky Pramana <ekkypramana@gmail.com>
2016-04-21 17:19:49 +0700

    Add question box placehoder (preview)

--- src/components/User.vue
+++ src/components/User.vue
@@ -5,7 +5,7 @@
     <div class="box mdl-cell mdl-cell--6-col mdl-cell--3-offset-desktop mdl-card-login mdl-card mdl-shadow--2dp">
       <h3>User Data</h3>
       <div>
-        <p v-link="'home'" v-if="user.authenticated">{{user.username}}</p>
+        <a class="mdl-navigation__link" v-link="'home'" v-if="user.authenticated">{{user.username}}</a>
       </div>
       <hr>
       <div class="quote-area" @click="getQuote()" v-if="quote">
@@ -13,7 +13,30 @@
       </div>
     </div>
   </div>
+
+  <!-- Question box placeholder -->
+  <div class="mdl-grid">
+    <div id="question-box" class="mdl-cell mdl-cell--6-col mdl-cell--3-offset-desktop mdl-card-login mdl-card mdl-shadow--2dp">
+      <div class="box-margin mdl-supporting-text">
+        <a class="mdl-navigation__link" v-link="'#'"><h5>Question<span class="mdl-badge" data-badge="12"></span></h5></a>
+      </div>
+      <div class="mdl-card__actions mdl-card--border"></div>
+      <div class="box-margin">
+        <a class="mdl-navigation__link" v-link="'home'">{this.author.username}</a><span> bertanya pada {this.updated_at} yang lalu.</span>
+      </div>
+    </div>
+  </div>
 </template>
+
+<style>
+#question-box {
+  min-height: 100px;
+}
+.box-margin {
+  margin: 0px 0px 10px 20px;
+}
+
+</style>
 
 <script>
 import auth from '../auth'
