48388066a994b22e76588a3b562192deb157970e
M Haidar Hanif <git@mhaidarhanif.com>
2016-04-21 14:58:00 +0700

    Create User component

---
+++ src/components/User.vue
@@ -0,0 +1,35 @@
+<!-- src/components/SecretQuote.vue -->
+
+<template>
+  <div class="col-sm-6 col-sm-offset-3">
+    <h1>User Data</h1>
+    <p>
+      {{ user }}
+    </p>
+  </div>
+</template>
+
+<script>
+import auth from '../auth'
+export default {
+  data() {
+    return {
+      quote: this.$http
+        .get('http://localhost:3030/api/protected/user', (data) => {
+          this.user = data;
+        }, {
+          // Attach the JWT header
+          headers: auth.getAuthHeader()
+        })
+        .error((err) => console.log(err))
+    }
+  },
+  route: {
+    // Check the users auth status before
+    // allowing navigation to the route
+    canActivate() {
+      return auth.user.authenticated
+    }
+  }
+}
+</script>
