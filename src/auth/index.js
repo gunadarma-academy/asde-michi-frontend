0ae9683a8fead83937e4984d752d30ea30df1d9e
M Haidar Hanif <git@mhaidarhanif.com>
2016-04-21 15:24:27 +0700

    Change port to 3001

--- src/auth/index.js
+++ src/auth/index.js
@@ -3,7 +3,7 @@
 import {router} from '../index'
 
 // URL and endpoint constants
-const API_URL = 'http://localhost:3030/'
+const API_URL = 'http://localhost:3001/'
 const SIGNIN_URL = API_URL + 'auth/local/'
 const SIGNUP_URL = API_URL + 'signup/'
 
